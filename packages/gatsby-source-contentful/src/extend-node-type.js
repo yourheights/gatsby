68372a1f330482a2440e052cce9c2b238876384d
axe312ger <opensource@axe312.dev>
2021-10-14 17:07:36 +0200

    fix(Contentful): respect gatsby-plugin-image defaults and extend and fix tests

--- packages/gatsby-source-contentful/src/extend-node-type.js
+++ packages/gatsby-source-contentful/src/extend-node-type.js
@@ -49,7 +49,6 @@ export async function setFieldsOnGraphQLNodeType({ type, cache }) {
         },
         quality: {
           type: GraphQLInt,
-          defaultValue: 50,
         },
       }
     )
